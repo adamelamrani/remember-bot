a0088c72e1d22d5de1eb11d476fa2e06e6ce69ff
Adam El Amrani Colina <adelamco1091@outlook.com>
2024-01-24 00:24:00 +0100

    Bot running locally

--- src/bot/telegramBot.ts
+++ src/bot/telegramBot.ts
@@ -1,16 +1,17 @@
 import "dotenv/config";
+const statusSelector = require('../utils/statusSelector');
 const { Telegraf } = require('telegraf');
 import { Context } from "telegraf";
 
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
 bot.command('oldschool', (ctx: Context) => ctx.reply('Hello'));
-bot.command('hipster', Telegraf.reply('Pepo'));
+bot.command('hipster', Telegraf.reply('actualizaçao'));
 
 try {
   bot.launch();
-  console.log("HOLA BUENAS")
+  console.log(statusSelector(200)(`Telegram bot is running`));
 } catch (error) {
-  console.log(error)
+  console.log(statusSelector(500)(`Telegram bot is not running`) + error);
 }
 
 // Enable graceful stop
