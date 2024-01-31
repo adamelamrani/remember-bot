febb8519308967ddb2139acbc89a67dcaf9f291b
Adam El Amrani Colina <adelamco1091@outlook.com>
2024-02-01 00:22:39 +0100

    Checkpoint to chatbot answers with context

--- src/bot/telegramBot.ts
+++ src/bot/telegramBot.ts
@@ -2,11 +2,50 @@ import "dotenv/config";
 import statusSelector from "../utils/statusSelector";
 import { Telegraf } from "telegraf";
 import { Context } from "telegraf";
+import { message } from 'telegraf/filters'
+
+interface MessageToSave {
+  username: string;
+  message: string;
+  date: number;
+
+}
+
+const saveMessage = (ctx: Context): MessageToSave => {
+  const messageToSave: MessageToSave = {
+    //@ts-ignore
+    username: ctx.message?.reply_to_message.from.username as string,
+    //@ts-ignore
+    message: ctx.message.reply_to_message.text as string,
+    //@ts-ignore
+    date: ctx.message.reply_to_message.date as number,
+  }
+  console.log(messageToSave);
+  return messageToSave;
+}
+
 
 
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY as string);
 bot.command('oldschool', (ctx: Context) => ctx.reply('Hello'));
 bot.command('hipster', Telegraf.reply('actualizaçao'));
+bot.help((ctx: Context) => ctx.reply('Send me a sticker')); //answers to /help
+
+//@ts-ignore
+bot.command("remember", (ctx: Context) => { saveMessage(ctx) }); //answers to /remember and saves the data
+bot.on(message('text'), async (ctx) => {
+  //@ts-ignore
+  if (ctx.message.reply_to_message?.text) {
+    //@ts-ignore
+    await ctx.reply(`Replied to message "${ctx.message.reply_to_message.text}" with: ${ctx.message.text}`)
+  } else {
+    await ctx.reply(`You said: ${ctx.message.text}`)
+  }
+
+  // console.log(ctx.update.message.from.username) //username
+  // console.log(ctx.message.text) //message
+  // console.log(ctx.message.date) //date
+})
 
 try {
   bot.launch();
