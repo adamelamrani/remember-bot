1d44caf9d8c7a4e6780569bbd3d94ddc4b4e872e
Adam El Amrani Colina <adelamco1091@outlook.com>
2024-02-01 11:15:27 +0100

    Change telegraf to telegram bot api?

--- src/routes/messagesRoutes.ts
+++ src/routes/messagesRoutes.ts
@@ -1,4 +1,3 @@
-export { }
 import { Request, Response } from 'express';
 import statusSelector from '../utils/statusSelector';
 const express = require('express');
@@ -6,7 +5,12 @@ const messagesRouter = express();
 
 messagesRouter.get('/', (req: Request, res: Response) => {
   console.log(statusSelector(res.statusCode)((`Resquest to endpoint "/messages" with status code ${res.statusCode}`)));
-  res.send('Hello World!');
+  res.send(JSON.stringify({ message: "Hello World" }));
+})
+
+messagesRouter.post('/', (req: Request, res: Response) => {
+  console.log(statusSelector(res.statusCode)((`Resquest to endpoint "/messages" with status code ${res.statusCode}`)));
+  res.send(JSON.stringify(`Este será el mensaje que se guardará en la BBDD: ${req.body}`));
 })
 
 export default messagesRouter;
